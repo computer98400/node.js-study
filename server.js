b9dd051a3d4c399bbc50a982630e2c79c0b443ee
computer98400 <computer98400@gmail.com>
2021-06-28 12:56:49 +0900

    setup express and study

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const http = require('http');
 
-const routes = require('./routes');
+const express = require('express');
 
-console.log(routes.someText);
-const server = http.createServer(routes.handler);
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('In the middleware');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('In another middleware');
+    res.send('<h1>Hello from Express!</h1>');
+});
+
+const server = http.createServer(app);
 
 server.listen(8080);
 
