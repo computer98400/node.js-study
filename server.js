b9bdf28aec6924d09583b69632dec99637f289fe
computer98400 <computer98400@gmail.com>
2021-06-29 23:53:57 +0900

    too late

--- server.js
+++ server.js
@@ -14,11 +14,13 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(shopRoutes);
 app.use(adminRoutes);
 
-// app.use('/', (req, res, next) => {
-//     console.log('This always runs');
-//     next();
 
-// })
+
+app.use( (req, res, next) => {
+
+    res.status(404).send('<h1>Page not found</h1>');
+
+})
 
 
 
