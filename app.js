e1b32544328e0301e5e616f5a3ccaedb3fea2dfb
computer98400 <computer98400@gmail.com>
2021-08-18 23:03:33 +0900

    모듈 내보내기 관련 내용

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const routes = require('./route');
 const http = require('http');
-const fs = require('fs');
+
+console.log(routes.someText);
 
 // function rqListener(req,res){
 
@@ -17,39 +19,7 @@ const fs = require('fs');
 //해당 구문을 그냥 표현할경우 아무것도 나오지 않는다.
 //- 단순히 서버를 만들어둔다해서 어떤 동작을 하는게 아니기 때문이다.
 
-const server = http.createServer((req, res) => {  //클라이언트의 요청에 따라서 행동을 정의하는 부분이라 생각해도 좋을 듯 싶다.
-    const url = req.url;
-    const method = req.method;
-    if(url === '/'){
-        res.write('<html>');        //응답에 일부 데이터를 쓸수있다.
-        res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-    if(url === '/message' && method ==='POST'){
-        const body = [] ;
-        req.on('data',(chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-        return req.on('end',() =>{
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, err =>{
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            })
-        });
-    }
-    res.setHeader('Content-Type','text/html'); 
-    res.write('<html>');        //응답에 일부 데이터를 쓸수있다.
-    res.write('<head><title>My First</title></head>');
-    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-    res.write('</html>');
-    res.end();
-});
+const server = http.createServer(routes);
 server.listen(3000);    //포트 3000을 통해 서버가 요청을 계속 수신하게 된다.
 
 //------------------------------------------------------
