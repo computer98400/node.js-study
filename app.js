8763abe197d5e5a3bedead2ef3242d12b7d6c70c
computer98400 <computer98400@gmail.com>
2021-08-20 23:14:37 +0900

    test

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-const routes = require('./route');
 const http = require('http');
+const express = require('express');
+const route = require('./route');
 
-console.log(routes.someText);
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+    next(); // Alows the request to continue t the next middleware in line
+});
+
+app.use((req, res, next) => {
+    console.log('test');
+
+});
+
+
+//console.log(routes.someText);
 
 // function rqListener(req,res){
 
@@ -19,7 +33,7 @@ console.log(routes.someText);
 //해당 구문을 그냥 표현할경우 아무것도 나오지 않는다.
 //- 단순히 서버를 만들어둔다해서 어떤 동작을 하는게 아니기 때문이다.
 
-const server = http.createServer(routes);
+const server = http.createServer(app);
 server.listen(3000);    //포트 3000을 통해 서버가 요청을 계속 수신하게 된다.
 
 //------------------------------------------------------
