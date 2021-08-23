c841b7035ad2ee8c7e346af33fba81f2f685c903
computer98400 <computer98400@gmail.com>
2021-08-23 23:13:57 +0900

    html style

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,15 @@
 const express = require('express');
-
+const path = require('path');
 const rotDir = require('../util/path');
 const router = express.Router();
 
-router.get('/admin/add-product',(req, res, next) => {
-    res.sendFile(path.join(rotDir,'views','add-product'));
+
+router.get('/add-product',(req, res, next) => {
+    res.sendFile(path.join(rotDir,'views','add-product.html'));
     // Alows the request to continue t the next middleware in line
 });
 //get || post || use || delete || patch || put....
-router.post('/admin/product', (req,res) => {
+router.post('/product', (req,res) => {
     //redirect  and File IO;
     console.log(req.url);
     console.log(req.method);
