d0b7002611bed5a1e9fb6ed95b02a574d9c075a2
computer98400 <computer98400@gmail.com>
2021-08-29 00:45:46 +0900

    readme

--- routes/shop.js
+++ routes/shop.js
@@ -6,8 +6,8 @@ const adminData = require('./admin');
 
 
 router.get('/', (req, res) => {
-    console.log('shop.js', adminData.products);
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', {prods: products, docTitle:'Shop'});
     //  res.send('<h1>Hello from express</h1>');
   });
   
