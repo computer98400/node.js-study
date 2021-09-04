664280173d19b8fe365af2403b35a26780f82563
computer98400 <computer98400@gmail.com>
2021-09-05 01:00:26 +0900

    ejs study

--- app.js
+++ app.js
@@ -7,11 +7,15 @@ const app = express();
 
 
 
-app.engine('handlebars', expressHbs({
-  layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'handlebars'
-})
-);
-app.set('view engine', 'handlebars');
+// app.engine('handlebars', expressHbs({
+//   layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'handlebars'
+// })
+// );
+
+
+
+app.set('view engine', 'ejs');
+//app.set('view engine', 'handlebars');
 //app.set('view engine', 'pug');
 app.set('views', 'views');
 
