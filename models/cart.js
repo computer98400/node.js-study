cbaed5698a02c2586203d06b7694b5f303f0a2cf
윤설 <computer98400@gmail.com>
2021-09-21 23:59:15 +0900

    create cart.js

---
+++ models/cart.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'cart.json'
+);
+
+module.exports = class Cart {
+    static addProduct(id, productPrice) {
+        fs.readFile(p, (err, fileContent) => {
+            let cart = { products: [], totalPrice: 0 };
+            if (!err) {
+                cart = JSON.parse(fileContent);
+            }
+
+            const existingProduct = cart.products.find(prod => prod.id === id);
+            let updatedProduct;
+
+
+            if (existingProduct) {
+                updatedProduct = { ...existingProduct };
+                updatedProduct.qty = updatedProduct.qty + 1;
+
+            } else {
+                updatedProduct = { id: id, qty: 1 };
+            }
+            cart.totalPrice = cart.totalPrice + productPrice;
+            cart.products = [...cart.products];
+
+        })
+
+    }
+    // constructor() {
+    //     this.products = [];
+    //     this.totalPrice = 0;
+    // }
+
+
+
+}
