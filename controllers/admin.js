0c1c2a0defee83280a7db992aef77d5cd4225d24
윤설 <computer98400@gmail.com>
2021-09-22 15:33:13 +0900

    edit-product 만들기

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,12 +1,10 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('admin/add-product', {
+    res.render('admin/edit-product', {
         pageTitle: 'Add Product',
-        path: 'admin/add-product',
-        formsCss: true,
-        productCss: true,
-        activeAddProduct: true
+        path: 'admin/edit-product',
+        editing: false
         // show: function () { console.log(pageTitle, path, formsCss, productCss, activeAddProduct) }
     });
     //res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
@@ -25,6 +23,27 @@ exports.postAddProduct = (req, res, next) => {
     res.redirect('/');
 }
 
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit;
+    if (!editMode) {
+        return res.redirect('/');
+    }
+    const prodId = req.params.productId;
+    Product.findById(prodId, product => {
+        if (!product) {
+            return res.redirect('/');
+        }
+        res.render('admin/edit-product', {
+            pageTitle: 'Edit Product',
+            path: 'admin/edit-product',
+            editing: editMode,
+            product: product
+        });
+
+    });
+
+
+}
 
 exports.getProducts = (req, res, next) => {
 
--- models/cart.js
+++ models/cart.js
@@ -15,19 +15,23 @@ module.exports = class Cart {
                 cart = JSON.parse(fileContent);
             }
 
-            const existingProduct = cart.products.find(prod => prod.id === id);
+            const existingProductIndex = cart.products.findIndex(prod => prod.id === id);
+            const existingProduct = cart.products[existingProductIndex];
             let updatedProduct;
-
 
             if (existingProduct) {
                 updatedProduct = { ...existingProduct };
                 updatedProduct.qty = updatedProduct.qty + 1;
-
+                cart.products = [...cart.products];
+                cart.products[existingProductIndex] = updatedProduct;
             } else {
                 updatedProduct = { id: id, qty: 1 };
+                cart.products = [...cart.products, updatedProduct];
             }
-            cart.totalPrice = cart.totalPrice + productPrice;
-            cart.products = [...cart.products];
+            cart.totalPrice = cart.totalPrice + +productPrice;
+            fs.writeFile(p, JSON.stringify(cart), err => {
+                console.log(err);
+            });
 
         })
 
